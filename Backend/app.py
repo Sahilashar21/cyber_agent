bd4d49da1fa5978d51f402c5094c1d18ca84321f
Sahilashar21 <sahilashar21@gmail.com>
2024-12-09 22:30:59 +0530

    my first project

---
+++ Backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from Tool1.app import tool1
+from Tool2.app import tool2
+
+
+
+app = Flask(__name__)
+
+# Register blueprints
+app.register_blueprint(tool1, url_prefix='/tool1')  # Accessible at /tool1
+app.register_blueprint(tool2, url_prefix='/tool2')  # Accessible at /tool2
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
